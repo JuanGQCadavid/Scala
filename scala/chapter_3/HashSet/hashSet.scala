fbbf72b27315e9121c4e8f3d8e073bfa2a33f1b2
Juan Cadavid <jquiro12@eafit.edu.co>
2017-01-18 23:50:45 -0500

    Set and HashSet

---
+++ scala/chapter_3/HashSet/hashSet.scala
@@ -0,0 +1,13 @@
+/*
+	Like the name says, is a has of Set.
+
+	Remember that Set is a trie.
+*/
+
+import scala.collection.immutable.HashSet
+
+val hashSet = HashSet("Tomatoes", "Chilies")
+
+println(hashSet + "Coriander")
+
+
--- scala/chapter_3/Set/set01.scala
+++ scala/chapter_3/Set/set01.scala
@@ -4,3 +4,49 @@
 
 	
 */
+
+
+var jetSet = Set("Boeing","Airbus")
+
+/*
+	jetSet is a Inmutable Scala set, becouse, is by default.
+	jetSet is Type Set[String]
+*/
+
+jetSet += "Lear"
+
+println(jetSet.contains("Cessna")) // Return if jetSet contains "Cessna"
+
+println(jetSet)
+
+jetSet -="Learg" // If Learg are not in Set, Scala Ommit.
+
+println(jetSet)
+
+jetSet -="Lear"
+
+println(jetSet)
+
+/*
+	On both mutable and	immutable sets, 
+	the + method will create and return 
+	a new set with the element added
+*/
+
+/*
+	Mutable Scala Get.
+*/
+
+import scala.collection.mutable
+
+val movieSet = mutable.Set("Hitch", "Poltergeist")
+
+movieSet += "Shrek"
+
+println(movieSet)
+
+/*
+	Mutables += is a Metohd , so movieSet.+=("Shrek")
+	Inmutables += is a simply ShortHand jetSet = jetSet + Cessna
+*/
+
